caa587201aca9f2377cf5aafd2db9ef5d5ccc7ba
jfernandzv <122486670+jfernandzv@users.noreply.github.com>
2023-01-28 22:08:05 +0100

    Ejercicio 1

---
+++ ejercicio1/index.js
@@ -0,0 +1,13 @@
+// base 16 * 23 cm
+//altura 13 cm
+
+const volumen = 16*23*13;
+console.log('volumen',volumen + ' cm3');
+//areaLateral 2 * 13 * (23 + 16) cm
+//areaBase = 16 * 23 cm
+//areaTotal = areaLateral + 2 * areaBase
+
+const areaLateral = 2*13*(23+16);
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log("Área total",areaTotal + ' cm2');
