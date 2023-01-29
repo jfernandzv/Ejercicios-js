5c25651d56739ed74437380d76a7114150a88d2a
jfernandzv <122486670+jfernandzv@users.noreply.github.com>
2023-01-29 21:28:03 +0100

    Ejercicio 7

--- ejercicio7/index.js
+++ ejercicio7/index.js
@@ -1,10 +1,13 @@
 const numeros = [1,2,3,4,5];
 
-let suma = 0;
-for (let valor of numeros) {
-    if (valor%2===0) {
-        suma += valor;
-    }    
+function sumaPares(numeros) {
+    let suma = 0;
+    for (let valor of numeros) {
+        if (valor%2===0) {
+            suma += valor;
+        }    
+    }
+    return suma;
 } 
 
-alert (`Suma de los pares = ${suma}`)
+alert (`Suma de los pares = ${sumaPares(numeros)}`)
