a46f4a7b13efcc32d71835e905ce31316ae97222
jfernandzv <122486670+jfernandzv@users.noreply.github.com>
2023-01-29 14:10:57 +0100

    Ejercicio 3

---
+++ ejercicio3/index.js
@@ -0,0 +1,8 @@
+const data = ["hola",2,5,"adiós"]
+console.log(`¿Es ${data[1]} el mayor?: ${data[1] > data[2]}`)
+console.log(`¿Es ${data[2]} el mayor?: ${data[2] > data[1]}`)
+console.log(`La suma de ${data[1]} + ${data[2]} es ${data[1] + data[2]}`)
+console.log(`La resta de ${data[1]} - ${data[2]} es ${data[1] - data[2]}`)
+console.log(`El producto de ${data[1]} * ${data[2]} es ${data[1] * data[2]}`)
+console.log(`La división de ${data[1]} / ${data[2]} es ${data[1] / data[2]}`)
+console.log(`El resto de ${data[2]} / ${data[1]} es ${data[2] % data[1]}`)
