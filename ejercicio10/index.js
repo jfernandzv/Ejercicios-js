188bca735bae7dcb7edb5adc7a3a9e9cc9d37233
jfernandzv <122486670+jfernandzv@users.noreply.github.com>
2023-02-01 20:11:04 +0100

    Ejercicio 10

--- ejercicio10/index.js
+++ ejercicio10/index.js
@@ -1,9 +1,9 @@
 function chekNumber(numero) {
-    let salida = ""
+    let salida = "";
     if (numero%2 === 0) {
-        salida = "Número par"
+        salida = "Número par";
     } else {
-        salida = "Número impar"
+        salida = "Número impar";
     }
-    return salida
+    return salida;
 }
